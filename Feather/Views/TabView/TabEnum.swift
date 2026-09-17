04f677520da4b2ecd3d96ee6e0afe5b42dff335b
ipa-black <ccf17ih@gmail.com>
2026-09-17 13:14:47 +0300

    Update TabEnum titles and icons

--- Feather/Views/TabView/TabEnum.swift
+++ Feather/Views/TabView/TabEnum.swift
@@ -10,55 +10,55 @@ import SwiftUI
 import NimbleViews
 
 enum TabEnum: String, CaseIterable, Hashable {
-	case home
-	case apps
-	case signing
-	case settings
-	case certificates
-	
-	var title: String {
-		switch self {
-		case .home:         return "الرئيسية"
-		case .apps:         return "التطبيقات"
-		case .signing:      return "التوقيع"
-		case .settings:     return "الإعدادات"
-		case .certificates: return "الشهادات"
-		}
-	}
-	
-	var icon: String {
-		switch self {
-		case .home:         return "house.fill"
-		case .apps:         return "square.grid.2x2.fill" // أيقونة التطبيقات المحدثة
-		case .signing:      return "signature"
-		case .settings:     return "gearshape.fill" // أيقونة الإعدادات المحدثة
-		case .certificates: return "checkmark.seal.fill"
-		}
-	}
-	
-	@ViewBuilder
-	static func view(for tab: TabEnum) -> some View {
-		switch tab {
-		case .home: HomeView() 
-		case .apps: SourcesView() 
-		case .signing: LibraryView()
-		case .settings: SettingsView()
-		case .certificates: NBNavigationView("الشهادات") { CertificatesView() }
-		}
-	}
-	
-	static var defaultTabs: [TabEnum] {
-		return [
-			.home,
-			.apps,
-			.signing,
-			.settings
-		]
-	}
-	
-	static var customizableTabs: [TabEnum] {
-		return [
-			.certificates
-		]
-	}
+    case home
+    case apps
+    case signing
+    case settings
+    case certificates
+    
+    var title: String {
+        switch self {
+        case .home:         return "اليوم"
+        case .apps:         return "المصادر"
+        case .signing:      return "التطبيقات"
+        case .settings:     return "الإعدادات"
+        case .certificates: return "الشهادات"
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .home:         return "macwindow"
+        case .apps:         return "cart.fill"
+        case .signing:      return "square.stack.3d.up.fill"
+        case .settings:     return "gearshape.fill"
+        case .certificates: return "checkmark.seal.fill"
+        }
+    }
+    
+    @ViewBuilder
+    static func view(for tab: TabEnum) -> some View {
+        switch tab {
+        case .home: HomeView() 
+        case .apps: SourcesView() 
+        case .signing: LibraryView()
+        case .settings: SettingsView()
+        case .certificates: NBNavigationView("الشهادات") { CertificatesView() }
+        }
+    }
+    
+    static var defaultTabs: [TabEnum] {
+        return [
+            .home,
+            .apps,
+            .signing,
+            .settings
+        ]
+    }
+    
+    static var customizableTabs: [TabEnum] {
+        return [
+            .certificates
+        ]
+    }
 }
